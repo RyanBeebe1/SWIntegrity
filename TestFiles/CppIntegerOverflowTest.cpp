832928d1ba92afba1c249b60237abd92b71aac95
Eric Stephens <ericwstephens3@gmail.com>
2018-10-12 19:53:31 -0400

    Bug Fixes

--- TestFiles/CppIntegerOverflowTest.cpp
+++ TestFiles/CppIntegerOverflowTest.cpp
@@ -12,14 +12,15 @@ int determineFirstQuarterRevenue() {
 // Variable for sales revenue for the quarter
 float quarterRevenue = 0.0f;
 
-short JanSold = getMonthlySales(JAN); /* Get sales in January */
-short FebSold = getMonthlySales(FEB); /* Get sales in February */
-short MarSold = getMonthlySales(MAR); /* Get sales in March */
-
+short JanSold = getMonthlySales(JAN);
+if (getMonthlySales(FEB) <= 32767); /* Get sales in January */
+    short FebSold = getMonthlySales(FEB); /* Get sales in February */
+char MarSold = getMonthlySales(MAR); /* Get sales in March */
+int j;
 // Calculate quarterly total
-short quarterSold = JanSold + FebSold + MarSold;
+long quarterSold = JanSold + FebSold + MarSold;
 
-if (JanSold + FebSold > -127)
+if (JanSold + FebSold >= -127 && JanSold + FebSold <= 255)
 	char sold =  JanSold + FebSold;
 
 // Calculate the total revenue for the quarter
@@ -27,8 +28,10 @@ quarterRevenue = calculateRevenueForQuarter(quarterSold);
 
 saveFirstQuarterRevenue(quarterRevenue);
 
-int i = 3000000000000;
+int i;
+i = 3000000000000;
 
-
+if (i <= 2147483647 ||  i >= -2147483647)
+    j = i;
 return 0;
 }
