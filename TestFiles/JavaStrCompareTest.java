e1993639b09030ba41f796947a613d370360d466
Joe Mcilvaine <jomac718@yahoo.com>
2018-10-26 11:17:12 -0400

    Added testing for java wrong String operator

---
+++ TestFiles/JavaStrCompareTest.java
@@ -0,0 +1,29 @@
+public class JavaStrCompareTest {
+
+
+    public static void main(String args[]) {
+        String str1 = new String("Hello");
+        String str2 = new String("Hello");
+        if (str1 != str2) {
+            System.out.println("str1 = str2");
+        }
+        String j = "yooosup";
+
+        String str = "gewrg";
+
+        if (j == str) {
+            //do something
+        }
+
+        String a = "werqwer";
+        String c = "eaqwww";
+
+        if (a.equals("")) {
+
+        }
+
+        if (a == "hghj") {
+
+        }
+    }
+}
