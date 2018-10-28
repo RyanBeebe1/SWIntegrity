453f9b82a1a7ad4c309c5a7b8b6b0c467cf3865a
Joe Mcilvaine <jomac718@yahoo.com>
2018-10-28 11:23:22 -0400

    Added test cases

---
+++ testClasses/JavaIncorrectStrOperatorTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class JavaIncorrectStrOperatorTest {
+    JavaAnalyzer jana;
+    JavaWrongStringOperator vuln;
+    @Before
+    public void setup() {
+        jana = new JavaAnalyzer();
+        vuln = new JavaWrongStringOperator();
+        jana.parse("./TestFiles\\JavaStrCompareTest.java");
+    }
+
+
+    @Test
+    public void test() {
+        List<Integer> locs = vuln.run(jana);
+        assertTrue(locs.size() == 4);
+        assertTrue(locs.contains(25));
+        assertTrue(locs.contains(7));
+        assertTrue(locs.contains(14));
+        assertTrue(!locs.contains(21));
+        assertTrue(locs.contains(28));
+    }
+}
